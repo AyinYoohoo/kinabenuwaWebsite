1eee42e6e2cf829d6d26aaa5bebeea69bc92fd7e
stelly <137718320+stylianimoses@users.noreply.github.com>
2025-06-26 09:52:40 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,12 +27,6 @@ document.addEventListener('click', (e) => {
     }
 });
 
-document.addEventListener('click', (e) => {
-    if (e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn") {
-        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
-    }
-});
-
 // Stop transition and animation during window resizing
 let resizeTimer;
 window.addEventListener('resize', () => {
