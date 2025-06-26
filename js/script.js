a8a387996e91c800e4a21d9f47c37ab2328e85f1
stelly <137718320+stylianimoses@users.noreply.github.com>
2025-06-26 09:43:13 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let navbarDiv = document.querySelector('.navbar'); 
 window.addEventListener('scroll', () => {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        navbarDiv.classList.add('navbar-cng');
+        navbarDiv.classList.add('navbar-cng'); // Add the class to navbar when scrolled
     } else {
-        navbarDiv.classList.remove('navbar-cng');
+        navbarDiv.classList.remove('navbar-cng'); // Remove the class when back at the top
     }
 });
 
@@ -16,9 +16,15 @@ navbarShowBtn.addEventListener('click', () => {
     navbarCollapseDiv.classList.add('navbar-collapse-rmw');
 });
 
-// Hide side bar
+// Hide sidebar
 navbarCloseBtn.addEventListener('click', () => {
     navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn") {
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
 });
 
 document.addEventListener('click', (e) => {
